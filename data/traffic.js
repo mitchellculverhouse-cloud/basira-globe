f5fb531b674a4e1b233bce5e27b7b90d27e67843
mitchellculverhouse-cloud <mitchellculverhouse@gmail.com>
2026-07-26 20:42:33 +0100

    Create traffic.js

---
+++ data/traffic.js
@@ -0,0 +1,22 @@
+const shippingTraffic = [
+    {
+        startLat:26.5,
+        startLng:56.3,
+        endLat:25.2,
+        endLng:55.3
+    },
+
+    {
+        startLat:12.6,
+        startLng:43.3,
+        endLat:30.0,
+        endLng:32.5
+    },
+
+    {
+        startLat:1.3,
+        startLng:103.8,
+        endLat:25.2,
+        endLng:55.3
+    }
+];
