3ae96f17695e6c0fc8cf2c78a9a00dff1580552c
mitchellculverhouse-cloud <mitchellculverhouse@gmail.com>
2026-07-26 19:44:29 +0100

    Create land.js

---
+++ data/land.js
@@ -0,0 +1,52 @@
+const landZones = [
+
+{
+name:"Europe",
+lat:50,
+lng:15,
+size:18
+},
+
+{
+name:"Middle East",
+lat:28,
+lng:45,
+size:12
+},
+
+{
+name:"South Asia",
+lat:20,
+lng:78,
+size:12
+},
+
+{
+name:"East Asia",
+lat:35,
+lng:120,
+size:18
+},
+
+{
+name:"Africa",
+lat:5,
+lng:20,
+size:25
+},
+
+{
+name:"North America",
+lat:45,
+lng:-100,
+size:25
+},
+
+{
+name:"South America",
+lat:-15,
+lng:-60,
+size:20
+}
+
+];
